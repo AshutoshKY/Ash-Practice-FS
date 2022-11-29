97c1cbbd605dac1394c208e0a76bb20e4737a671
AshutoshKY <ashuyadav2554@gmail.com>
2022-11-29 16:33:26 +0530

    MySql Data Push & Retrieve

--- 29th_Nov/mysql/index.js
+++ 29th_Nov/mysql/index.js
@@ -4,7 +4,10 @@ var mysql = require("mysql");
 const bodyParser=require("body-parser")
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/views'));
 
+//Connect MySql Database by crating a variable con which
+//has databse name and other metadata
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -12,23 +15,47 @@ var con = mysql.createConnection({
     database:"hello"
 });
 
+// Shows if database with specified name is Conneted ot not
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
+// Console Logs * from table specified in query
+con.connect(function(err) {
+    con.query("SELECT * FROM saved", function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+
+// Render index.js at "localhost:3000/"
 app.get("/",(req,res) => {
     res.render("index");
 });
 
+//Render and shows all Data from Specified table using data.ejs
+app.get('/data', function(req, res) {
+    con.query('SELECT * FROM saved', function (err, result) {
+      if (err) throw err;
+
+      res.render('data', {data: result});
+    });
+  });
+
+//Connexts with a form to push Data ot Specified table using
+//insert query and console log result
 app.post("/push_data",(req,res)=>{
-    var data=req.body.push;
+    var Name=req.body.push1;
+    var Roll_No=req.body.push2;
+    var Marks=req.body.push2;
+    // var data=req.body.push;
     // console.log(data);
-    var sql = "INSERT INTO save (sl_no, Name) VALUES ?";
-    var values = [[ ,data]];
+    var sql = "INSERT INTO saved (sl_no, Name, Roll_No, Marks) VALUES ?";
+    var values = [[ ,Name,Roll_No,Marks]];
     con.query(sql, [values], function (err, result) {
         if (err) throw err;
-        console.log("1 Record inserted");
+        console.log("Record inserted "+ result.affectedRows);
     });
 });
 
