79ff6203aae624ef8902d53dede72487ca590f75
Ashutosh KY <ashuyadav2554@gmai.com>
2023-02-22 19:10:44 +0530

    22nd_Feb-Fe-UseEffect

--- 20th_Feb_FE_UseState-GIT/src/Classes/20th_feb.jsx
+++ 20th_Feb_FE_UseState-GIT/src/Classes/20th_feb.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './style.css';
 
 const First20feb = () => {
     
     const [count, setCount] = useState(0);
+    const [com, setCom] = useState("Ashutosh");
+    // const [back,setBack]=useState(true);
+
+    useEffect(() => {
+       console.log("CLicked Number Chnage");
+    },[count]);
+
     const func = (a) =>{
         if(a===1){
             setCount(count+1)
@@ -22,10 +29,18 @@ const First20feb = () => {
                 <h1>Basics</h1>
             </div>
             <div className="mainContent">
-                <h2>Count is : {count}</h2>
-                <button className="btns" onClick={() => func(2)}>Decrease</button>
-                <button className="btns" onClick={() => func(1)}>Increase</button>
-                <button className="btns" onClick={() => func(3)}>Reset</button>
+                <div className="left">
+                    <h2 id="cnt" style={count>10 ? {color:"green", textDecoration:"underline"}:{color:"rgb(126, 41, 70);"}}>Count is : {count}</h2>
+                    <button className="btns" onClick={() => func(2)}>Decrease</button>
+                    <button className="btns" onClick={() => func(1)}>Increase</button>
+                    <button className="btns" onClick={() => func(3)}>Reset</button>
+                </div>
+                <div className="right">
+                    {/* 22nd Class  */}
+                <h3>{com}</h3>
+                    <button className="btns" onClick={()=>{com==="Ashutosh" ? setCom("kumar"):setCom("Ashutosh")}}>change</button>
+                    <p>1977, pagol</p>
+                </div>
             </div>
         </div>
     );
